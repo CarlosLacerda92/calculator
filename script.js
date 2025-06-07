1f0031c2729951bce586bda4dc69f8b3543222bf
Carlos Lacerda <148892466+CarlosLacerda92@users.noreply.github.com>
2025-06-07 15:37:49 -0300

    feat: handle new calculation after finishing the previous one

--- script.js
+++ script.js
@@ -63,13 +63,19 @@ function handleOperationClick(operationSelected) {
 
     if (operations.includes(operationSelected)) {
 
+        if (!calculationData.number1 && !calculationData.number2 && !calculationData.operation && calculationData.lastResult) {
+            calculationData.number1 = calculationData.lastResult;
+        }
+
         if (calculationData.number1 && calculationData.number2 && calculationData.operation) {
             operate();
             calculationData.number1 = calculationData.lastResult;
         }
 
         calculationData.operation = operationSelected;
+
         printIntoDisplay(operationDisplay, `${calculationData.number1} ${calculationData.operation}`);
+
         return;
     }
 
